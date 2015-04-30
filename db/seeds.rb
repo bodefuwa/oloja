dfd0d58babe35bf2def44561e85e2ac7f264f9c3
Bode Fuwa <bodefuwa@gmail.com>
2015-04-30 16:56:07 -0400

    Product scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+# ...
+Product.create!([{
+  title: 'Ankara',
+  description: 
+    %(<p>
+    	high quality, designs
+      </p>),
+  image_url: 'ankara.jpg',
+  price: 89.95
+},
+{
+  title: 'Gele',
+  description: 
+    %(<p>
+    	latest head gear. Vibrant colors
+      </p>),
+  image_url: 'gele.jpg',
+  price: 79.95
+},
+{
+  title: 'Shoe and Bag',
+  description: 
+    %(<p>
+        matching shoes and bags, leather, high quality
+      </p>),
+  image_url: 'shoe.jpg',
+  price: 49.95
+},
+])
