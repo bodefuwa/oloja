1ad4287977471b35c8750ec718361fcdc310ac53
Bode Fuwa <bodefuwa@gmail.com>
2015-05-02 10:40:22 -0400

    Added atom feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :baskets
+  
   get 'shop/index'
-  resources :products
+  
+  resources :products do
+  	get :who_bought, on: :member
+  end
+  
   root 'shop#index', as: 'shop'
 end
